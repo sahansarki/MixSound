f49163d077ea71291ac72461903bc7272902de75
Gökhan ÖZTÜRK <gokhanozturk@androidedu.io>
2020-02-09 13:31:43 +0300

    First commit from Forked Account

--- app/src/main/java/live/codemy/mixsound/MainActivity.kt
+++ app/src/main/java/live/codemy/mixsound/MainActivity.kt
@@ -1,5 +1,14 @@
 package live.codemy.mixsound
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
